3665a0d0681a6f2e6a56ee6afc78fbb0a2e61099
Caspar Krieger <git@asparck.com>
2020-05-11 16:44:17 -0700

    add tap dances

--- keyboards/kinesis/keymaps/caspark/keymap.c
+++ keyboards/kinesis/keymaps/caspark/keymap.c
@@ -5,8 +5,24 @@ enum kinesis_layers {
   _TEMPLATE,  // template layer for easy copy pasting to make new layers (not a real layer)
 };
 
-//Aliases for longer keycodes
-#define CTLESC  CTL_T(KC_ESC)
+// Tap dancing setup - https://docs.qmk.fm/#/feature_tap_dance
+enum {
+  TD_COLON = 0,
+  TD_LBRACK,
+  TD_RBRACK,
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_COLON]  = ACTION_TAP_DANCE_DOUBLE(KC_COLN, KC_SCLN),
+  [TD_LBRACK]  = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LPRN),
+  [TD_RBRACK]  = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RPRN),
+};
+
+// Aliases for longer keycodes
+//     "12345678" is a visual guide to help ensure aliases are 8 characters or less so they fit
+#define CTLESC   CTL_T(KC_ESC)
+#define COLONS   TD(TD_COLON)
+#define LBRACKS  TD(TD_LBRACK)
+#define RBRACKS  TD(TD_RBRACK)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_pretty( // default base layer
@@ -14,12 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_ESC  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_F7   ,KC_F8   /**/,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,KC_PSCR ,KC_SLCK ,KC_PAUS ,KC_INS  ,RESET
 ,KC_EQL  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5                               /**/                           ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS
 ,KC_TAB  ,KC_Q    ,KC_W    ,KC_F    ,KC_P    ,KC_G                               /**/                           ,KC_J    ,KC_L    ,KC_U    ,KC_Y    ,KC_SCLN ,KC_BSLS
-,CTLESC  ,KC_A    ,KC_R    ,KC_S    ,KC_T    ,KC_D                               /**/                           ,KC_H    ,KC_N    ,KC_E    ,KC_I    ,KC_O    ,KC_QUOT
+,KC_CAPS ,KC_A    ,KC_R    ,KC_S    ,KC_T    ,KC_D                               /**/                           ,KC_H    ,KC_N    ,KC_E    ,KC_I    ,KC_O    ,KC_QUOT
 ,KC_LSPO ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B                               /**/                           ,KC_K    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSPC
-         ,KC_GRV  ,KC_LGUI ,KC_LBRC ,KC_RBRC                                     /**/                                    ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
-                                                               ,KC_CAPS ,KC_LALT /**/,KC_RGUI ,KC_RCTL
+         ,KC_GRV  ,KC_LGUI ,LBRACKS ,RBRACKS                                     /**/                                    ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
+                                                               ,KC_CAPS ,KC_LALT /**/,COLONS ,KC_RCTL
                                                                         ,KC_LALT /**/,KC_PGUP
-                                                      ,KC_SPC  ,KC_LCTRL,KC_DEL  /**/,KC_PGDN ,KC_ENTER,KC_BSPC
+                                                      ,KC_SPC  ,CTLESC  ,KC_DEL  /**/,KC_PGDN ,KC_ENTER,KC_BSPC
 )
 
 ,[_TEMPLATE] = LAYOUT_pretty( // template layer, for easy copy pasting to make a new layer
--- keyboards/kinesis/keymaps/caspark/readme.md
+++ keyboards/kinesis/keymaps/caspark/readme.md
@@ -1,1 +1,5 @@
-# The default keymap for kinesis-advantage
+# Caspar's keymap
+
+Inspired by https://asylum.madhouse-project.org/blog/2016/10/15/multi-purpose-keys/
+and by http://www.pvv.org/~hakonhal/main.cgi/keyboard/arensito_devel/
+and https://github.com/qmk/qmk_firmware/tree/master/keyboards/kinesis/keymaps/tuesdayjohn
