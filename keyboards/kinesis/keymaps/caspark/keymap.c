50cc95ebfb3de9253871237292ecb4a31b2cae44
Caspar Krieger <git@asparck.com>
2020-05-15 13:20:00 -0700

    move mouse button 2 and 3 on GUI layer so that ctrl click is possible

--- keyboards/kinesis/keymaps/caspark/keymap.c
+++ keyboards/kinesis/keymaps/caspark/keymap.c
@@ -129,9 +129,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ,_______ ,XXXXXXX ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_WH_D                            /**/                           ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
 ,_______ ,_______ ,_______ ,CLS_TAB ,_______ ,_______                            /**/                           ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
          ,_______ ,_______ ,_______ ,CLS_WIN                                     /**/                                    ,_______ ,_______ ,_______ ,_______
-                                                               ,KC_ACL0 ,_______ /**/,_______ ,_______
+                                                               ,KC_ACL0 ,KC_BTN3 /**/,_______ ,_______
                                                                         ,_______ /**/,_______
-                                                      ,KC_BTN1 ,KC_BTN2 ,KC_BTN3 /**/,_______ ,_______ ,_______
+                                                      ,KC_BTN1 ,_______ ,KC_BTN2 /**/,_______ ,_______ ,_______
 )
 
 ,[LAYER_TEMPLATE] = LAYOUT_pretty( // template layer, for easy copy pasting to make a new layer
