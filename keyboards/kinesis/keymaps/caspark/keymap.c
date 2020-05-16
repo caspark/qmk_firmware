b185e8480f64a51f68c35a5dfc1af1b91a54778a
Caspar Krieger <git@asparck.com>
2020-05-15 20:14:06 -0700

    allow GUI layer to override gaming layer

--- keyboards/kinesis/keymaps/caspark/keymap.c
+++ keyboards/kinesis/keymaps/caspark/keymap.c
@@ -125,6 +125,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       ,_______ ,_______ ,_______ /**/,_______ ,_______ ,WRDBSPC
 )
 
+,[LAYER_GAMING] = LAYOUT_pretty( // gaming layer - WASD is moved one row to the right, with TGB moving to where QAZ is normally (and stuff like GRV becomes LCTL)
+//______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
+ _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
+,_______ ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5                               /**/                           ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______
+,_______ ,KC_T    ,KC_Q    ,KC_W    ,KC_E    ,KC_R                               /**/                           ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,_______
+,_______ ,KC_G    ,KC_A    ,KC_S    ,KC_D    ,KC_F                               /**/                           ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,_______
+,_______ ,KC_B    ,KC_Z    ,KC_X    ,KC_C    ,KC_V                               /**/                           ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,_______
+         ,KC_LCTL ,KC_LALT ,KC_LEFT ,KC_RGHT                                     /**/                                    ,KC_UP   ,KC_DOWN ,KC_LBRC ,KC_RBRC
+                                                               ,KC_G    ,_______ /**/,_______ ,_______
+                                                                        ,_______ /**/,_______
+                                                      ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______
+)
+
 ,[LAYER_GUI] = LAYOUT_pretty( // mouse keys and other utilities
 //______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
  _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
@@ -136,19 +149,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                ,KC_ACL0 ,KC_BTN3 /**/,_______ ,_______
                                                                         ,_______ /**/,_______
                                                       ,KC_BTN1 ,_______ ,KC_BTN2 /**/,_______ ,_______ ,_______
-)
-
-,[LAYER_GAMING] = LAYOUT_pretty( // gaming layer - WASD is moved one row to the right, with TGB moving to where QAZ is normally (and stuff like GRV becomes LCTL)
-//______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
- TRU_ESC ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_F7   ,KC_F8   /**/,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,KC_PSCR ,KC_SLCK ,KC_PAUS ,KC_INS  ,LAY_ADJ
-,KC_EQL  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5                               /**/                           ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS
-,KC_TAB  ,KC_T    ,KC_Q    ,KC_W    ,KC_E    ,KC_R                               /**/                           ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS
-,KC_CAPS ,KC_G    ,KC_A    ,KC_S    ,KC_D    ,KC_F                               /**/                           ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT
-,KC_LSFT ,KC_B    ,KC_Z    ,KC_X    ,KC_C    ,KC_V                               /**/                           ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT
-         ,KC_LCTL ,KC_LALT ,KC_LEFT ,KC_RGHT                                     /**/                                    ,KC_UP   ,KC_DOWN ,KC_LBRC ,KC_RBRC
-                                                               ,KC_G    ,KC_LALT /**/,KC_RGUI  ,KC_RCTL
-                                                                        ,KC_HOME /**/,KC_PGUP
-                                                      ,KC_SPC  ,KC_DEL  ,KC_END  /**/,KC_PGDN ,KC_ENT  ,KC_BSPC
 )
 
 ,[LAYER_QWERTY] = LAYOUT_pretty( // qwerty layout, where every key does what it says on the keyboard (except maybe one or two of the function keys)
