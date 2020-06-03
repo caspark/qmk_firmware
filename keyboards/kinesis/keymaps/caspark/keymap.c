20c8686c9374cb490ae347cd8e7f601effca8108
Caspar Krieger <git@asparck.com>
2020-06-02 19:55:22 -0700

    Move B back to qwerty position
    V is still relatively easy to reach at qwerty T position on the kinesis,
    given that control is on a thumb key at the moment, and this means less
    adjustment.

--- keyboards/kinesis/keymaps/caspark/keymap.c
+++ keyboards/kinesis/keymaps/caspark/keymap.c
@@ -119,9 +119,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ /**/,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______ ,_______
  TRU_ESC ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,KC_F6   ,KC_F7   ,KC_F8   /**/,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,KC_PSCR ,KC_SLCK ,KC_PAUS ,KC_INS  ,LAY_ADJ
 ,KC_EQL  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5                               /**/                           ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS
-,KC_TAB  ,KC_Q    ,KC_W    ,KC_F    ,KC_P    ,KC_B                               /**/                           ,KC_J    ,KC_L    ,KC_U    ,KC_Y    ,KC_SCLN ,KC_BSLS
+,KC_TAB  ,KC_Q    ,KC_W    ,KC_F    ,KC_P    ,KC_V                               /**/                           ,KC_J    ,KC_L    ,KC_U    ,KC_Y    ,KC_SCLN ,KC_BSLS
 ,OSL_GUI ,KC_A    ,KC_R    ,KC_S    ,KC_T    ,KC_G                               /**/                           ,KC_M    ,KC_N    ,KC_E    ,KC_I    ,KC_O    ,KC_QUOT
-,KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_D    ,KC_V                               /**/                           ,KC_K    ,KC_H    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_DEL
+,KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_D    ,KC_B                               /**/                           ,KC_K    ,KC_H    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_DEL
          ,KC_GRV  ,KC_LGUI ,KC_LBRC ,KC_RBRC                                     /**/                                    ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
                                                                ,LAY_EDT ,KC_CAPS /**/,XXXXXXX ,TAB_GUI
                                                                         ,ONE_ALT /**/,KC_PGUP
